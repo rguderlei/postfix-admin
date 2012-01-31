9fcd258df96090c7d8b1424288d3dfadd43fc971
Ralph Guderlei <ralph.guderlei@gmail.com>
2012-01-31 19:36:06 +0100

    basic concept for ember.js lists

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,7 @@
 var App = Ember.Application.create();
 
+
+
 App.Forwarding = Ember.Object.extend({
     source: "",
     destination: "",
@@ -20,23 +22,34 @@ App.ForwardingListEntry = Ember.View.extend({
   templateName: 'forwarding-list-entry'
 });
 
+var capitalize = function(aString){
+  return aString.charAt(0).toUpperCase() + aString.slice(1);
+}
+
 App.Mailbox = Ember.Object.extend({
     email: "",
     password: "",
     password_confirmation: "",
+
     name: function(){
+        //if( this.get("email") != "") {
+        //    var parts = this.get("email").split("@")[0].split["."];
+        //    return capitalize(parts[1]) + ", " + capitalize(parts[0]);
+        //}
+
         return this.get("email").split("@")[0];
+
     }.property("email")
 });
 
-var m1 = App.Mailbox.create({source: "ralph.guderlei@mvbiberach.de"});
-var m2 = App.Mailbox.create({source: "karin.guderlei@mvbiberach.de"});
+var m1 = App.Mailbox.create({email: "ralph.guderlei@mvbiberach.de"});
+var m2 = App.Mailbox.create({email: "karin.guderlei@mvbiberach.de"});
 
 App.MailboxController = Ember.Object.create({
    mailboxes: [m1, m2]
 });
 
-App.MailbxoxListEntry = Ember.View.extend({
+App.MailboxListEntry = Ember.View.extend({
   classNames: ["row"],
   templateName: 'mailbox-list-entry'
 });
